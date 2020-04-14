c962d6df47af9ddee997e649c43fbb56d82bacd6
Alexandr <efimchiksasha@gmail.com>
2020-04-14 18:53:37 +0300

    fix POST

--- app.js
+++ app.js
@@ -38,6 +38,18 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + '/public'));
  
+//bodyParser
+// Обратите внимание на используемый путь. Именно он задается в атрибуте action формы
+app.use('/form_data', bodyParser.urlencoded({
+    extended: true
+}));
+
+// Обратите внимание на используемый путь. Именно он задается в атрибуте action формы
+app.post('/form_data', function(req, res, next) {
+    // Объект req.body содержит данные из переданной формы
+    console.log(req.body);
+});
+
 app.use("/", function(request, response){
      
     response.render("tasks", {
@@ -54,17 +66,6 @@ app.use("/", function(request, response){
     response.send("Please follow the port 3001!");
 });
 
-//bodyParser
-// Обратите внимание на используемый путь. Именно он задается в атрибуте action формы
-app.use('/form_data', bodyParser.urlencoded({
-    extended: true
-}));
-
-// Обратите внимание на используемый путь. Именно он задается в атрибуте action формы
-app.post('/form_data', function(req, res, next) {
-    // Объект req.body содержит данные из переданной формы
-    console.log(req.body);
-});
 
 app.listen(3001, function() {
     console.log("Server was started");
